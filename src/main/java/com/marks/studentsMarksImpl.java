9a789d3428c84c1a0adce5bb7b67b18add60b28b
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-30 13:14:54 +0530

    final commit

--- src/main/java/com/marks/studentsMarksImpl.java
+++ src/main/java/com/marks/studentsMarksImpl.java
@@ -7,5 +7,12 @@ package com.marks;
 
 public class studentsMarksImpl {
     public static void main(String[] args) {
-
+        StudentMarks studentMarks = new StudentMarks();
+        studentMarks.calculateMarksForMedicalPathway(76,98,70,79);
+        studentMarks.calculateMarksForNonMedicalPathway(87,87,89);
+        studentMarks.calculateMarksForBusinessPathWay(78,87,88);
+        System.out.println("calculateMarksForBusinessPathWay = " + studentMarks.calculateMarksForBusinessPathWay(78,87,88));
+        System.out.println("studentMarks.calculateMarksForMedicalPathway = " + studentMarks.calculateMarksForMedicalPathway(76,98,70,79));
+        System.out.println("studentMarks.calculateMarksForNonMedicalPathway = " + studentMarks.calculateMarksForNonMedicalPathway(87,87,89));
+    }
 }
