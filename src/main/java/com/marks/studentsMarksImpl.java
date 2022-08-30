d18c033ec6a1292b4f8aee218a14a1edfdecba8a
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-30 13:14:39 +0530

    the impl class is created

---
+++ src/main/java/com/marks/studentsMarksImpl.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name: Philip Meshach
+ * Date: 30-08-2022
+ * Praise The Lord
+ */
+package com.marks;
+
+public class studentsMarksImpl {
+    public static void main(String[] args) {
+
+}
