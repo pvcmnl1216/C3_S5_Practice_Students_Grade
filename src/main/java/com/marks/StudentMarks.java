63a4749bc047b5a37f9fba4bc48bb5a28a688218
Philip Meshach <philipmeshach.pm@gmail.com>
2022-08-30 12:52:34 +0530

    the methods for calculating the marks is created

--- src/main/java/com/marks/StudentMarks.java
+++ src/main/java/com/marks/StudentMarks.java
@@ -6,5 +6,20 @@
 package com.marks;
 
 public class StudentMarks {
+public int calculateMarksForMedicalPathway(int maths, int physics, int chemistry, int biology){
 
+    int totalmarks = (maths+physics+chemistry+biology);
+    int sum = totalmarks;
+    return sum ;
+}
+public int calculateMarksForNonMedicalPathway(int maths,int physics,int chemistry){
+    int totalmarks = (maths+physics+chemistry);
+    int sum = totalmarks;
+    return sum;
+}
+public int calculateMarksForBusinessPathWay(int businessstudies,int finance ,int accounting){
+    int totalmarks = (businessstudies+finance+accounting);
+    int sum = totalmarks;
+    return sum;
+}
 }
